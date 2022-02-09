16e0b764a81d5c1d2423e56007411270ca9b53f7
FLAMiNGPHYtON1 <thehighscorer@gmail.com>
2022-02-09 22:43:06 +0800

    Files

---
+++ src/main/java/com/mycompany/assignment_project_2/Login_Details.java
@@ -0,0 +1,95 @@
+
+package com.mycompany.assignment_project_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.swing.JOptionPane;
+
+public class Login_Details 
+{
+    private String User_ID;
+    private String Password;
+    private String Role;
+    
+    public void Login_Verification(String U_ID, String U_PS, String U_RL)
+    {
+        this.User_ID = U_ID;
+        this.Password = U_PS;
+        this.Role = U_RL;
+        int found = 0;
+        try 
+        {
+            Path p = Paths.get(".", "User_Details.txt");   
+            BufferedReader reader = Files.newBufferedReader(p);
+            String line;
+
+            while ((line = reader.readLine()) != null) 
+            {
+                String[] fields = line.split("[,]");
+                if (User_ID.equals(fields[0]) && Password.equals(fields[2]) && Role.equals(fields[3]) ) 
+                {
+                    found = 1;
+                    if (Role.equals("Admin"))
+                    {
+                        Menu_Admin Obj3 = new Menu_Admin();
+                        JOptionPane.showMessageDialog(null, "NOICCEEE ADMINIY BOI");                
+                        Obj3.setLocationRelativeTo(null);
+                        Obj3.setVisible(true);
+                    }
+                    else if (Role.equals("Technician"))
+                    {
+                        Menu_Technician Obj4 = new Menu_Technician();
+                        
+                        JOptionPane.showMessageDialog(null, "NOICCEEE TECHY BOI");                
+                        Obj4.setLocationRelativeTo(null);
+                        Obj4.setVisible(true);
+                    }
+                    else
+                    {
+                        Menu_Customer Obj5 = new Menu_Customer();                      
+                        JOptionPane.showMessageDialog(null, "NOICCEEE CUSTOMER BOI");                
+                        Obj5.setLocationRelativeTo(null);
+                        Obj5.setVisible(true);
+                    }                   
+                }  
+                
+                    /*for (int i = 0; i < fields.length; ++i) 
+                    {
+                        System.out.println(i + ": " + fields[i]);
+                    }
+                    
+                    name.setText(fields[1]);
+                    age.setText(fields[2]);
+                    gender.setText(fields[3]);
+                    dep.setText(fields[4]);
+                    nation.setText(fields[5]);
+                    email.setText(fields[6]);
+                    phone.setText(fields[7]);
+                    add.setText(fields[8]);
+                    city.setText(fields[9]);
+                    state.setText(fields[10]);
+                    */
+
+            }
+                //writer.write(String.join(",", fields));
+                //writer.newLine();
+        }
+        catch (IOException ex){}  
+        if (found == 0)
+        {
+          JOptionPane.showMessageDialog(null, "NO RECORD FOUND LAH WTF YOU FUCK");  
+        }
+    }
+        // copy new file & delete temporary file
+        //Files.copy(tempFile, p, StandardCopyOption.REPLACE_EXISTING);
+       // Files.delete(tempFile);
+}
+        
+              
+
+
+      
+
---
+++ src/main/java/com/mycompany/assignment_project_2/Registration_Details.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.assignment_project_2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Registration_Details 
+{
+    private String User_ID;
+    private String Contact_Number;
+    private String Password;
+    private String Role;
+    
+    public void register(String U_ID, String Ctc_Num, String U_PS, String RL)
+    {
+        this.User_ID = U_ID;
+        this.Contact_Number = Ctc_Num;
+        this.Password = U_PS;
+        this.Role = RL;
+        
+        FileWriter User_Details;
+        try 
+        {
+            User_Details = new FileWriter("User_Details.txt",true);
+            User_Details.write(User_ID + ",");
+            User_Details.write(Contact_Number + ",");
+            User_Details.write(Password + ",");
+            User_Details.write(Role);
+            User_Details.write(System.getProperty("line.separator"));
+            User_Details.close();
+        } 
+        catch (IOException ex){}
+            
+    }   
+}
